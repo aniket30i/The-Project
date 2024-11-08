f38bc245b035d3b4e0743d426e9305b114aa625e
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-11-08 16:38:21 +0530

    plus and minus

---
+++ src/components/UI/Hero.jsx
@@ -0,0 +1,7 @@
+import { useEffect } from "react";
+const Hero = () => {
+  useEffect(() => {});
+  return <div>Hi</div>;
+};
+
+export default Hero;
---
+++ src/components/main/FullApp.jsx
@@ -0,0 +1,11 @@
+import Hero from "../UI/Hero";
+
+const FullApp = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default FullApp;
